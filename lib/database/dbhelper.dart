f21ca552323ec17dc3d49c1521c6c0a1e91dd07b
nitis <nitis.182546@gmail.com>
2025-02-15 02:32:11 +0700

    add insert select all

---
+++ lib/database/dbhelper.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_crud/person.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:sqflite/sqlite_api.dart';
+import 'package:path/path.dart';
+
+class Dbhelper {
+  Dbhelper._instance();
+  static final Dbhelper instance = Dbhelper._instance();
+  static Database? _database;
+
+  Future<Database> get db async {
+    _database = await initDB();
+    return _database!;
+  }
+
+  Future<Database> initDB() async {
+    String databasePath = await getDatabasesPath();
+    String path = join(databasePath, 'emp.db');
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute(
+      'CREATE TABLE employees(id INTEGER PRIMARY KEY,name TEXT,department TEXT)',
+    );
+  }
+
+  Future<int> insertEmp(Person emp) async {
+    Database db = await instance.db;
+    return await db.insert(
+      'employees',
+      emp.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Map<String,dynamic>>> queryAll() async {
+    Database db = await instance.db;
+    return await db.query('employees');
+  }
+}
